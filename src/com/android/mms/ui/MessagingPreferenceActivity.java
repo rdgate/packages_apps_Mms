3e60c25707a5da023fbe6b597874116585060bdd
Andrew Warfield <hiasant@gmail.com>
2013-10-06 15:12:17 -0700

    Fix derp
    Add the extra }

--- src/com/android/mms/ui/MessagingPreferenceActivity.java
+++ src/com/android/mms/ui/MessagingPreferenceActivity.java
@@ -799,4 +799,6 @@ public class MessagingPreferenceActivity extends PreferenceActivity
     public static boolean getBreathEnabled(Context context) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
         return prefs.getBoolean(MessagingPreferenceActivity.NOTIFICATION_BREATH, false);
+    }
+    
 }
