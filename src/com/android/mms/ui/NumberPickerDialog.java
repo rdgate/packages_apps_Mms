6c4bcab4eee19043be19ad313595e2bf5d49d806
Alexandre Pesant <alex@theboredengineers.com>
2013-11-25 06:53:01 +0100

    Mms: cofigurable text edit field height
    Ported from:
    	"Added parameter in Mms app to set the maximum number of lines the textEdit will expand to"
    	by Alexandre Pesant <alex@theboredengineers.com>
    	http://review.cyanogenmod.org/51442
    
    Change-Id: I20c09f5a4fd828e360c095aabf2aea3ad035b928
    
    Conflicts:
    
    	res/values/cm_strings.xml

--- src/com/android/mms/ui/NumberPickerDialog.java
+++ src/com/android/mms/ui/NumberPickerDialog.java
@@ -24,6 +24,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.NumberPicker;
+import android.widget.TextView;
 
 import com.android.mms.R;
 
@@ -47,6 +48,7 @@ public class NumberPickerDialog extends AlertDialog implements OnClickListener {
 
     private final NumberPicker mNumberPicker;
     private final OnNumberSetListener mCallback;
+    private final TextView mSummaryTextView;
 
     /**
      * @param context Parent.
@@ -58,8 +60,10 @@ public class NumberPickerDialog extends AlertDialog implements OnClickListener {
             int number,
             int rangeMin,
             int rangeMax,
-            int title) {
-        this(context, AlertDialog.THEME_HOLO_LIGHT, callBack, number, rangeMin, rangeMax, title);
+            int title,
+            int summary) {
+        this(context, AlertDialog.THEME_HOLO_LIGHT, callBack, number,
+                rangeMin, rangeMax, title, summary);
     }
 
     /**
@@ -74,7 +78,8 @@ public class NumberPickerDialog extends AlertDialog implements OnClickListener {
             int number,
             int rangeMin,
             int rangeMax,
-            int title) {
+            int title,
+            int summary) {
         super(context, theme);
         mCallback = callBack;
 
@@ -88,6 +93,8 @@ public class NumberPickerDialog extends AlertDialog implements OnClickListener {
                 (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View view = inflater.inflate(R.layout.number_picker_dialog, null);
         setView(view);
+        mSummaryTextView =(TextView) view.findViewById(R.id.number_picker_summary);
+        mSummaryTextView.setText(summary);
         mNumberPicker = (NumberPicker) view.findViewById(R.id.number_picker);
 
         // initialize state
