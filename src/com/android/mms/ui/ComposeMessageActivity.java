551cca2e02b1a1bc6cca1a814cec0c03a9a7a39f
yanglv <yanglv@codeaurora.org>
2014-08-28 12:04:30 +0800

    Mms: Add copy URL function in message.
    Add a function to copy URL into clipboard in message item menu.
    
    Change-Id: I888c6ab8b68dbeec662df6aed91c97804fdacdeb
    
    Conflicts:
    	src/com/android/mms/ui/ComposeMessageActivity.java

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -249,6 +249,7 @@ public class ComposeMessageActivity extends Activity
     private static final int MENU_ADD_TEMPLATE          = 34;
     private static final int MENU_ADD_TO_BLACKLIST      = 35;
     private static final int MENU_ADD_TO_CALENDAR       = 36;
+    private static final int MENU_COPY_EXTRACT_URL      = 37;
 
     private static final int DIALOG_TEMPLATE_SELECT     = 1;
     private static final int DIALOG_TEMPLATE_NOT_AVAILABLE = 2;
@@ -1047,20 +1048,33 @@ public class ComposeMessageActivity extends Activity
                 uriString = uriString.substring(sep + 1);
             }
             Uri contactUri = null;
+            String extractedUrl = null;
+            boolean isUrl = false;
             boolean knownPrefix = true;
             if ("mailto".equalsIgnoreCase(prefix))  {
                 contactUri = getContactUriForEmail(uriString);
             } else if ("tel".equalsIgnoreCase(prefix)) {
                 contactUri = getContactUriForPhoneNumber(uriString);
+            } else if ("http".equalsIgnoreCase(prefix) || "https".equalsIgnoreCase(prefix)) {
+                extractedUrl = prefix + ":" + uriString;
+                isUrl = true;
             } else {
                 knownPrefix = false;
             }
-            if (knownPrefix && contactUri == null) {
+            if (knownPrefix && contactUri == null && !isUrl) {
                 Intent intent = ConversationList.createAddContactIntent(uriString);
 
                 String addContactString = getString(R.string.menu_add_address_to_contacts,
                         uriString);
                 menu.add(0, MENU_ADD_ADDRESS_TO_CONTACTS, 0, addContactString)
+                    .setOnMenuItemClickListener(l)
+                    .setIntent(intent);
+            } else if (isUrl) {
+                String copyurl = getString(R.string.menu_copy_url,
+                        extractedUrl);
+                Intent intent = new Intent();
+                intent.putExtra("copyurl", extractedUrl);
+                menu.add(0, MENU_COPY_EXTRACT_URL, 0, copyurl)
                     .setOnMenuItemClickListener(l)
                     .setIntent(intent);
             }
@@ -1442,6 +1456,11 @@ public class ComposeMessageActivity extends Activity
                     addEventToCalendar(mMsgItem.mSubject, mMsgItem.mBody);
                     return true;
                 }
+
+                case MENU_COPY_EXTRACT_URL:
+                    String copyedUrl = item.getIntent().getStringExtra("copyurl");
+                    copyToClipboard(copyedUrl);
+                    return true;
 
                 default:
                     return false;
