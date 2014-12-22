b43d33a3e8b0abd752d994b4a5ea5e4c45a32e70
Martin Brabham <optedoblivion@cyngn.com>
2014-12-22 15:21:44 -0800

    Mms: Add close button for removing subject as a new UX path instead of relying on the depracated onKeyListener
    - Vector FTW
    
    Change-Id: I69b1b95cd01f38d3801e33eff6a3dd9c4941fab6
    
    Conflicts:
    	src/com/android/mms/ui/ComposeMessageActivity.java

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -73,6 +73,8 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SqliteWrapper;
 import android.drm.DrmStore;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
 import android.gesture.Gesture;
 import android.gesture.GestureLibrary;
@@ -80,6 +82,7 @@ import android.gesture.GestureOverlayView;
 import android.gesture.GestureOverlayView.OnGesturePerformedListener;
 import android.gesture.Prediction;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.VectorDrawable;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -136,6 +139,7 @@ import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
@@ -310,7 +314,9 @@ public class ComposeMessageActivity extends Activity
     private TextView mTextCounter;          // Shows the number of characters used in text editor
     private TextView mSendButtonMms;        // Press to send mms
     private ImageButton mSendButtonSms;     // Press to send sms
+    private LinearLayout mSubjectWrapper;   // Wrapp for subject and cancel button
     private EditText mSubjectTextEditor;    // Text editor for MMS subject
+    private ImageView mSubjectRemoveButton; // Remove the subject and editor
 
     private AttachmentEditor mAttachmentEditor;
     private View mAttachmentEditorScrollView;
@@ -2109,18 +2115,31 @@ public class ComposeMessageActivity extends Activity
             log("" + show);
         }
 
+        if (mSubjectWrapper == null) {
+            mSubjectWrapper = (LinearLayout) findViewById(R.id.subject_wrapper);
+        }
+
         if (mSubjectTextEditor == null) {
-            // Don't bother to initialize the subject editor if
-            // we're just going to hide it.
-            if (show == false) {
-                return;
-            }
             mSubjectTextEditor = (EditText)findViewById(R.id.subject);
             mSubjectTextEditor.setFilters(new InputFilter[] {
                     new LengthFilter(MmsConfig.getMaxSubjectLength())});
         }
 
-        mSubjectTextEditor.setOnKeyListener(show ? mSubjectKeyListener : null);
+        if (mSubjectRemoveButton == null) {
+            mSubjectRemoveButton = (ImageView) findViewById(R.id.subject_cancel);
+            // Adding this removal button so we don't have to rely on the broken on key listener
+            // which only works on select third party soft IMEs or hardware keyboards from back
+            // in the old days. Lollipop LatinIME specifies that it will refuse to send backward
+            // compat updown key events for anything less than jelly bean.
+            mSubjectRemoveButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mWorkingMessage.setSubject(null, true);
+                    showSubjectEditor(false);
+                    updateSendButtonState();
+                }
+            });
+        }
 
         if (show) {
             mSubjectTextEditor.addTextChangedListener(mSubjectEditorWatcher);
@@ -2128,8 +2147,11 @@ public class ComposeMessageActivity extends Activity
             mSubjectTextEditor.removeTextChangedListener(mSubjectEditorWatcher);
         }
 
+        mSubjectTextEditor.setOnKeyListener(show ? mSubjectKeyListener : null);
         mSubjectTextEditor.setText(mWorkingMessage.getSubject());
-        mSubjectTextEditor.setVisibility(show ? View.VISIBLE : View.GONE);
+
+        mSubjectWrapper.setVisibility(show ? View.VISIBLE : View.GONE);
+
         hideOrShowTopPanel();
     }
 
@@ -2734,8 +2756,8 @@ public class ComposeMessageActivity extends Activity
     }
 
     private boolean isSubjectEditorVisible() {
-        return (null != mSubjectTextEditor)
-                    && (View.VISIBLE == mSubjectTextEditor.getVisibility());
+        return (null != mSubjectWrapper)
+                    && (View.VISIBLE == mSubjectWrapper.getVisibility());
     }
 
     @Override
